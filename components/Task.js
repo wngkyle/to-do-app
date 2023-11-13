6848f2232863ca79fe53153316bcbb73cdb764e8
wngkyle <99611120+wngkyle@users.noreply.github.com>
2023-11-13 00:52:13 -0500

    done

--- components/Task.js
+++ components/Task.js
@@ -5,9 +5,7 @@ const Task = (props) => {
     return (
         <View style={styles.item}>
             <View style={styles.itemLeft}>
-                <TouchableOpacity style={styles.square}>
-
-                </TouchableOpacity>
+                <View style={styles.square}></ View>
                 <Text style={styles.itemText}>
                     {props.taskName}
                 </Text>
@@ -25,23 +23,33 @@ const styles = StyleSheet.create({
         backgroundColor: "#FFF",
         padding: 15,
         borderRadius: 10,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        marginBottom: 20,
     },
     itemLeft: {
         flexDirection: 'row',
         alignItems: 'center',
         flexWrap: 'wrap',
     },
-    circular: {
-
-    },
     square: {
         width: 24,
         height: 24,
         backgroundColor: '#55BCF6',
         opacity: 0.4,
+        borderRadius: 5,
+        marginRight: 15,
     },
     itemText: {
-
+        maxWidth: '80%',
+    },
+    circular: {
+        width: 12,
+        height: 12,
+        borderColor: '#55BCF6',
+        borderWidth: 2,
+        borderRadius: 5,
     },
 });
 
